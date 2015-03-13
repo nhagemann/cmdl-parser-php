d342e869301a176842b3a22d8ad5d331b4db4622
Nils Hagemann <mail@nilshagemann.de>
2015-03-13 21:46:31 +0100

    final wording options of annotation sortable

--- src/CMDL/Annotations/DataTypeSortableAnnotation.php
+++ src/CMDL/Annotations/DataTypeSortableAnnotation.php
@@ -10,22 +10,24 @@ class DataTypeSortableAnnotation extends Annotation
 
     protected $annotationType = 'sortable';
 
-
     public function apply()
     {
         if ($this->hasParam(1))
         {
             switch ($this->getParam(1))
             {
-                case 'no':
+                case 'not':
                     $this->dataTypeDefinition->setSortable(false);
                     break;
                 case 'list':
                 case 'tree':
                     $this->dataTypeDefinition->setSortable($this->getParam(1));
                     break;
+                case 'related':
+                    $this->dataTypeDefinition->setSortable($this->getParam(1));
+                    break;
                 default:
-                    throw  new CMDLParserException('Parameter "type" of annotation ' . $this->annotationType . ' must be one of no,list,tree.', CMDLParserException::CMDL_INVALID_OPTION_VALUE);
+                    throw  new CMDLParserException('Parameter "type" of annotation ' . $this->annotationType . ' must be one of not,list,tree, related.', CMDLParserException::CMDL_INVALID_OPTION_VALUE);
                     break;
             }
 
