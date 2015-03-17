69d92f1bb3fb3e81fedbfdcc561a78ed6e804099
Nils Hagemann <mail@nilshagemann.de>
2015-03-17 09:44:11 +0100

    added getter for all lists and params of a form element

--- src/CMDL/FormElementDefinition.php
+++ src/CMDL/FormElementDefinition.php
@@ -4,6 +4,7 @@ namespace CMDL;
 
 class FormElementDefinition
 {
+
     protected $elementType = null;
 
     protected $name = null;
@@ -12,12 +13,10 @@ class FormElementDefinition
 
     protected $defaultValue = null;
 
-
     protected $help = null;
     protected $hint = null;
     protected $info = null;
     protected $placeholder = null;
-
 
     protected $mandatory = false;
     protected $unique = false;
@@ -47,54 +46,45 @@ class FormElementDefinition
         $this->name = $name;
     }
 
-
     public function getName()
     {
         return $this->name;
     }
-
 
     public function setLabel($label)
     {
         $this->label = $label;
     }
 
-
     public function getLabel()
     {
         return $this->label;
     }
-
 
     public function setDefaultValue($defaultValue)
     {
         $this->defaultValue = $defaultValue;
     }
 
-
     public function getDefaultValue()
     {
         return $this->defaultValue;
     }
-
 
     public function markMandatory()
     {
         $this->mandatory = true;
     }
 
-
     public function isMandatory()
     {
         return (boolean)$this->mandatory;
     }
 
-
     public function markUnique()
     {
         $this->unique = true;
     }
-
 
     public function isUnique()
     {
@@ -106,102 +96,95 @@ class FormElementDefinition
         $this->protected = true;
     }
 
-
     public function isProtected()
     {
         return (boolean)$this->protected;
     }
-
 
     public function hasParam($nr)
     {
         return isset($this->params[$nr - 1]);
     }
 
-
     public function hasList($nr)
     {
         return isset($this->lists[$nr - 1]);
     }
 
+    public function getLists()
+    {
+        return $this->lists;
+    }
+
+    public function getParams()
+    {
+        return $this->params;
+    }
 
     public function getParam($nr)
     {
 
-        if ($this->hasParam($nr))
-        {
+        if ($this->hasParam($nr)) {
             return $this->params[$nr - 1];
         }
 
         return null;
     }
 
-
     public function getList($nr)
     {
 
-        if ($this->hasList($nr))
-        {
+        if ($this->hasList($nr)) {
             return $this->lists[$nr - 1];
         }
 
         return null;
     }
 
-
     public function getMaxValueLength()
     {
         return $this->maxValueLength;
     }
-
 
     public function setHelp($help)
     {
         $this->help = $help;
     }
 
-
     public function getHelp()
     {
         return $this->help;
     }
-
 
     public function setHint($hint)
     {
         $this->hint = $hint;
     }
 
-
     public function getHint()
     {
         return $this->hint;
     }
-
 
     public function setInfo($info)
     {
         $this->info = $info;
     }
 
-
     public function getInfo()
     {
         return $this->info;
     }
-
 
     public function setPlaceholder($placeholder)
     {
         $this->placeholder = $placeholder;
     }
 
-
     public function getPlaceholder()
     {
         return $this->placeholder;
     }
-
 
     public function setInsertedByInsert($insertName)
     {
@@ -217,5 +200,4 @@ class FormElementDefinition
     {
         return $this->insertedByInsert;
     }
-
 }
